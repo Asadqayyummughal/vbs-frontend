30d4300d543fbbd9fc0e24974a9680c06f8fb13a
Asad Qayyum <asadqayyumbscs@gmail.com>
2024-01-06 20:18:25 +0500

    style:Add more sections on services page

--- src/app/frontend/frontend-routing.module.ts
+++ src/app/frontend/frontend-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FrontendComponent } from './frontend.component';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 const routes: Routes = [
   { 
@@ -10,16 +11,21 @@ const routes: Routes = [
     children:[
       {
       path:"home",
-      component:HomeComponent
+      component:ServicesComponent,
       },
       {
         path:"",
         redirectTo:"/home",
         pathMatch:"full"
       },
+      // {
+      //   path:"services",
+      //   redirectTo:"services",
+      //   component:ServicesComponent,
+      // },
       {
         path:"**",
-        redirectTo:"/home"
+        redirectTo:"/services"
       }
     ]}];
 
